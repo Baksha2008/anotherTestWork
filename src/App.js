b291d44b37f30b2d0c31a0c522cefe95dcd1f6a1
baksha2008 <baksha2008@gmail.com>
2018-12-26 09:21:48 +0200

    small fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { QuestionPage } from './components/pages/questionPage/questionPage';
 import { AnswerPage } from './components/pages/answerPages/answerPage'
 
@@ -9,8 +9,9 @@ export class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/questions-page" component={ QuestionPage } />
-          <Route path="/answer-page" component={ AnswerPage } />
+          <Redirect exact from='/' to='/questions-page' />
+          <Route exact path="/questions-page" component={QuestionPage} />
+          <Route path="/answer-page" component={AnswerPage} />
         </Switch>
       </BrowserRouter>
     );
